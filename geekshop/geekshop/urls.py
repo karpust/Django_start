ced0bb9c3f1ec707913d8d9a6d2e87bafb37b6ac
karpust <956277@mail.ru>
2021-11-16 09:49:59 +0300

    homework by lesson 7

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -22,12 +22,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', mainapp.main, name='main'),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('contact/', mainapp.contact, name='contact'),
     path('shop/', include('mainapp.urls', namespace='shop')),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
     path('products/', include('mainapp.urls', namespace='products')),
+    path('admin/', include('adminapp.urls', namespace='admin')),
 
 ]
 
