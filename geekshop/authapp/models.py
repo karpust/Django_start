09464249758d5de1ed2cf084fa19882fb7df94b8
karpust <956277@mail.ru>
2022-08-28 18:10:59 +0300

    lesson 7

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
-    age = models.PositiveIntegerField(verbose_name='Возраст')
+    age = models.PositiveIntegerField(verbose_name='Возраст', null=False)
     first_name = models.CharField(verbose_name='Имя', max_length=30)
